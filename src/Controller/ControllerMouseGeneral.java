9d96467cbd40c44bc8d3e8e0f798487362a5a667
Tommaso <tommasopirozzi@hotmail.it>
2018-05-16 11:18:51 +0200

    Aggiunto MouseControllerGeneral

---
+++ src/Controller/ControllerMouseGeneral.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author Pirozzi
+ */
+public class ControllerMouseGeneral extends MouseAdapter{
+    //dichiarate model e view
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        //questoè il controller per i click.
+        //qua fate quello che deve fare. 
+       
+    }
+    
+   
+    
+}
