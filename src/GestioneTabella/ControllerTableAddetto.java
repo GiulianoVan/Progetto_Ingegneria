04f20bfd245bc0e376e04b5cf50ca06981035d32
vincenzotor <vincenzo916@hotmail.it>
2018-05-30 20:04:56 +0200

    fix tabella addetto su INVIO

--- src/GestioneTabella/ControllerTableAddetto.java
+++ src/GestioneTabella/ControllerTableAddetto.java
@@ -39,7 +39,7 @@ public class ControllerTableAddetto extends ControllerTableGeneral{
         MyDefaultTableModel tab = (MyDefaultTableModel) view.getTableSearchGeneral().getModel();
         //SE PREMO INVIO E LA CELLA è EDITABILE.FAI UPDATE
         //ROW E COLUMN = -1 POICHE SE ENTRO QUI ,NON HO + LA CELLA EDITABILE
-        if(e.getKeyChar()=='\n' && view.getTableSearchGeneral().isCellEditable(row, column)) 
+        if(e.getKeyChar()=='\n' && view.getTableSearchGeneral().isCellEditable(row, column) && row != -1 && column != -1) 
         {
             try {
                 tab.setColumnEditable(-1);
